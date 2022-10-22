daee6ee64807c59e02a226cf09b65ab278d17d26
Lior Banai <liorbanai@gmail.com>
2022-10-22 16:04:26 +0300

    update nugets

--- Analogy.LogViewer.WordsSearch/IAnalogy/WordsSearchUserSettingsFactory.cs
+++ Analogy.LogViewer.WordsSearch/IAnalogy/WordsSearchUserSettingsFactory.cs
@@ -5,17 +5,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Analogy.Interfaces;
 using Analogy.LogViewer.WordsSearch.Managers;
 
 namespace Analogy.LogViewer.WordsSearch.IAnalogy
 {
-    public class WordsSearchUserSettingsFactory: UserSettingsFactory
+    public class WordsSearchUserSettingsFactory: TemplateUserSettingsFactory
     {
         public override string Title { get; set; } = "Words Search settings";
-        public override UserControl DataProviderSettings { get; set; } = new WordsSearchSettings();
+        public override UserControl DataProviderSettings { get; set; }
 
         public override Guid FactoryId { get; set; } = PrimaryFactory.Id;
         public override Guid Id { get; set; } = new Guid("38de920b-0a7b-4152-a3b2-a67a07a74082");
+
+        public override void CreateUserControl(IAnalogyLogger logger)
+        {
+            DataProviderSettings = new WordsSearchSettings();
+        }
 
         public override Task SaveSettingsAsync()
         {
